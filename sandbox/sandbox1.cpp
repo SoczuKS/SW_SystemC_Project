dba4c15d687afaff88a07d316d64e1bd92054758
SoczuKS <soczynskik@gmail.com>
2025-03-30 15:03:37 +0200

    Init project with CMake and SystemC

---
+++ sandbox/sandbox1.cpp
@@ -0,0 +1,22 @@
+#include <systemc>
+
+void hello1() {
+    std::cout << "Hello world using approach 1" << std::endl;
+}
+
+struct HelloWorld : sc_core::sc_module {
+    SC_CTOR(HelloWorld) {
+        SC_METHOD(hello2);
+    }
+
+    void hello2(void) {
+        std::cout << "Hello world using approach 2" << std::endl;
+    }
+};
+
+int sc_main(int, char *[]) {
+    hello1();
+    HelloWorld helloworld("helloworld");
+    sc_core::sc_start();
+    return 0;
+}
